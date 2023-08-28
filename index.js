83389e9ab8eec75efc993cd5331616daf3111034
cocktailpeanut <cocktailpeanuts@proton.me>
2023-08-28 19:19:03 -0400

    amd fix

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ class Automatic1111 {
     // if SDXL (not legacy) => "--no-download-sd-model"
     let defaultArgs = (legacy ? "" : "--no-download-sd-model ")
     if (platform === 'darwin') {
-      if (vendors.includes("apple")) {
+      let test = vendors.filter((vendor) => {
+        return /apple/i.test(vendor)
+      }).length > 0
+      if (test) {
 //      if (/apple/i.test(vendor)) {
         defaultArgs += "--skip-torch-cuda-test --upcast-sampling --use-cpu interrogate --no-half --api"
       } else {
@@ -26,17 +29,27 @@ class Automatic1111 {
       let newtext = text.replace(re, `$2"${defaultArgs}"`)
       await fs.promises.writeFile(path.resolve(__dirname, "automatic1111", "webui-user.sh"), newtext)
     } else if (platform === 'win32') {
-      defaultArgs += "--xformers --no-half-vae --api"
+      let test = vendors.filter((vendor) => {
+        return /advanced micro devices/i.test(vendor)
+      }).length > 0
+      if (test) {
+        defaultArgs += "--no-half-vae --api"
+      } else {
+        defaultArgs += "--xformers --no-half-vae --api"
+      }
       let text = await fs.promises.readFile(path.resolve(__dirname, "automatic1111", "webui-user.bat"), "utf8")
       let re = /^(set COMMANDLINE_ARGS=)(.*)$/m
       let newtext = text.replace(re, `$1${defaultArgs}`)
       await fs.promises.writeFile(path.resolve(__dirname, "automatic1111", "webui-user.bat"), newtext)
     } else {
       // linux
-      if (vendors.includes("advanced micro devices")) {
+      let test = vendors.filter((vendor) => {
+        return /advanced micro devices/i.test(vendor)
+      }).length > 0
+      if (test) {
       //if (/amd/i.test(vendor)) {
         // lshqqytiger
-        defaultArgs += "--precision full --no-half-vae --xformers --api"
+        defaultArgs += "--precision full --no-half-vae --api"
       } else {
         defaultArgs += "--xformers --no-half-vae --api"
       }
